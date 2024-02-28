a7c2870137e66c3d97e891b38fe4227230b2dbdc
dkwapisz <dkwapisz99@gmail.com>
2024-02-28 21:01:36 +0100

    Swagger docs

--- src/main/java/com/pk/lab2/controller/ProductController.java
+++ src/main/java/com/pk/lab2/controller/ProductController.java
@@ -2,7 +2,15 @@ package com.pk.lab2.controller;
 
 import com.pk.lab2.model.Product;
 import com.pk.lab2.model.ProductDTO;
+import com.pk.lab2.model.ProductSummaryDTO;
 import com.pk.lab2.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,8 +21,10 @@ import static com.pk.lab2.validation.ProductValidation.isProductValidForCreate;
 import static com.pk.lab2.validation.ProductValidation.isProductValidForUpdate;
 import static java.util.Objects.nonNull;
 
+
 @RestController
 @RequestMapping(("/api/products"))
+@Tag(name = "Products management API")
 public class ProductController {
 
     private final ProductService productService;
@@ -24,6 +34,13 @@ public class ProductController {
     }
 
     @GetMapping
+    @Operation(summary = "Get list of all products with short details")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returning list of all products with short details",
+                    content = {@Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = ProductSummaryDTO.class)))}),
+            @ApiResponse(responseCode = "500", description = "Internal server error - something is wrong on server side",
+                    content = @Content)})
     public ResponseEntity<?> getAllProducts() {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(productService.getAllProducts());
@@ -33,6 +50,15 @@ public class ProductController {
     }
 
     @GetMapping("/{productId}")
+    @Operation(summary = "Get product by Id with all details")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returning product by given Id with long details",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Product.class))}),
+            @ApiResponse(responseCode = "404", description = "Product with given Id not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error - something is wrong on server side",
+                    content = @Content)})
     public ResponseEntity<?> getProductById(@PathVariable String productId) {
         try {
             Product product = productService.getProductById(productId);
@@ -47,12 +73,20 @@ public class ProductController {
     }
 
     @PostMapping
+    @Operation(summary = "Create new product")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Product has been created",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Product.class))}),
+            @ApiResponse(responseCode = "400", description = "Received invalid values in some of Product fields",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error - something is wrong on server side",
+                    content = @Content)})
     public ResponseEntity<?> createProduct(@RequestBody ProductDTO productDTO) {
         try {
             if (!isProductValidForCreate(productDTO)) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Collections.emptyMap());
             }
-
             return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(productDTO));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
@@ -60,7 +94,20 @@ public class ProductController {
     }
 
     @PatchMapping("/{productId}")
-    public ResponseEntity<?> updateProduct(@PathVariable String productId, @RequestBody ProductDTO productDTO) {
+    @Operation(summary = "Update existing product by Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Product with given Id has been updated",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Product.class))}),
+            @ApiResponse(responseCode = "400", description = "Received invalid values in some of Product fields",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Product with given Id not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error - something is wrong on server side",
+                    content = @Content)})
+    public ResponseEntity<?> updateProduct(@PathVariable String productId, @io.swagger.v3.oas.annotations.parameters
+            .RequestBody(description = "Updated product details", required = true,
+            content = @Content(schema = @Schema(implementation = ProductDTO.class))) @RequestBody ProductDTO productDTO) {
         try {
             if (!isProductValidForUpdate(productDTO)) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Collections.emptyMap());
@@ -78,6 +125,14 @@ public class ProductController {
     }
 
     @DeleteMapping("/{productId}")
+    @Operation(summary = "Delete existing product by Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Product with given Id has been deleted",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Product with given Id not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error - something is wrong on server side",
+                    content = @Content)})
     public ResponseEntity<?> deleteProduct(@PathVariable String productId) {
         try {
             boolean deleted = productService.deleteProduct(productId);
--- src/main/java/com/pk/lab2/model/ProductDTO.java
+++ src/main/java/com/pk/lab2/model/ProductDTO.java
@@ -1,18 +1,31 @@
 package com.pk.lab2.model;
 
 import com.pk.lab2.enums.ProductCategory;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@Schema(description = "Product Data Transfer Object")
 public class ProductDTO {
 
+    @Schema(description = "Product name", example = "This is item name", minLength = 1)
     private String name;
+
+    @Schema(description = "Product description", example = "This is description of item", minLength = 1)
     private String description;
+
+    @Schema(description = "Product quantity", example = "100", minimum = "1", maximum = "Infinity")
     private Integer quantity;
+
+    @Schema(description = "Product price", example = "34.62", minimum = "0.01", maximum = "Infinity")
     private Float price;
+
+    @Schema(description = "Product weight", example = "534.21", minimum = "0.01", maximum = "Infinity")
     private Float weight;
+
+    @Schema(description = "Product category", example = "ELECTRONICS")
     private ProductCategory productCategory;
 
 }
